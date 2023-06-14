d05c684047b342ff9ae519cbae08525ed1f4dc5c
ZharkoILIEVSKI <zarko.ilievski@gmail.com>
2023-06-14 22:14:55 +0200

    Add project files.

---
+++ HW-1/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        [Route("https://localhost:7252/ListOrders")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("https://localhost:7252/Order/Details{id?}")]
+        public IActionResult Details(int? id)
+        {
+            if (id == 0)
+            {
+                return new EmptyResult();
+            }
+            else
+            {
+                return View();
+            } 
+        }
+
+        [Route("https://localhost:7252/Order/JsonData")]
+        public IActionResult GetJson()
+        {
+            var order = new { id = 3, IsDelivered = false };
+            return new JsonResult(order);
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ HW-1/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult GetPizzas() 
+        {
+            var pizzas = StaticDb.Pizzas;
+            return View(pizzas);
+        }
+    }
+}
