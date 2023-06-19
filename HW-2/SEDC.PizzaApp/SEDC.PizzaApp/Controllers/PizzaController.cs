95d9a4d0a35d8ff49bc72c8c0fcfebf34b5002df
ZharkoILIEVSKI <zarko.ilievski@gmail.com>
2023-06-20 00:00:22 +0200

    домашна работа бр.2

---
+++ HW-2/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/PizzaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<PizzaViewModel> pizzaList = new List<PizzaViewModel>()
+            {
+                new PizzaViewModel
+                {
+                    Id = 1,
+                    Name = StaticDb.Pizzas.First().Name,
+                    Price = StaticDb.Pizzas.First().Price,
+                    PizzaSize = StaticDb.Pizzas.First().PizzaSize
+                },
+                new PizzaViewModel
+                {
+                    Id = 2,
+                    Name = StaticDb.Pizzas.Last().Name,
+                    Price = StaticDb.Pizzas.Last().Price,
+                    PizzaSize = StaticDb.Pizzas.Last().PizzaSize
+                }
+            };
+            
+            return View(pizzaList);
+        }
+        public IActionResult GetPizzas() 
+        {
+            var pizzas = StaticDb.Pizzas;
+            return View(pizzas);
+        }
+
+        public IActionResult Details(PizzaViewModel id)
+        {
+            if (!(id == null))
+            {
+                return View();
+            }
+            else
+            return new EmptyResult();
+        }
+    }
+}
---
+++ HW-2/SEDC.PizzaApp/SEDC.PizzaApp/Mappers/OrderMapper.cs
@@ -0,0 +1,20 @@
+﻿using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Mappers
+{
+    public static class OrderMapper
+    {
+        public static OrderViewModel ToOrderViewModel(Order order)
+        {
+            return new OrderViewModel()
+            {
+                Id = order.Id,
+                FirstName = order.User.FirstName,
+                LastName = order.User.LastName,
+                PhoneNumber = order.User.PhoneNumber,
+                UserAddress = order.User.Address,
+            };
+        }
+    }
+}
